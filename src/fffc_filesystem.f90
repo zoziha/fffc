fb409bd3328ad800c419e74aec6c1a538069c5ef
zoziha <zuo.zhihua@qq.com>
2023-03-30 17:25:52 +0800

    添加rmdir、mkdir

--- src/fffc_filesystem.f90
+++ src/fffc_filesystem.f90
@@ -3,5 +3,11 @@ module fffc_filesystem
         module function is_windows()
             logical :: is_windows
         end function is_windows
+        module subroutine mkdir(path)
+            character(len=*), intent(in) :: path
+        end subroutine mkdir
+        module subroutine rmdir(path)
+            character(len=*), intent(in) :: path
+        end subroutine rmdir
     end interface
 end module fffc_filesystem
