188ad6a98a6928d849c824182f48f2677ac44c7f
zoziha <zuo.zhihua@qq.com>
2023-03-30 17:21:18 +0800

    添加is_windows

---
+++ src/fffc_filesystem.f90
@@ -0,0 +1,7 @@
+module fffc_filesystem
+    interface
+        module function is_windows()
+            logical :: is_windows
+        end function is_windows
+    end interface
+end module fffc_filesystem
--- src/fffc_module.f90
+++ src/fffc_module.f90
@@ -1,4 +1,6 @@
 module fffc_module
+    use fffc_constants
+    use fffc_filesystem
     use fffc_kinds
     use fffc_math
 end module fffc_module
