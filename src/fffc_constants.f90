27ee152710e5e40147e3ed86ba877ccfad609ba5
zoziha <zuo.zhihua@qq.com>
2023-03-30 17:13:58 +0800

    添加pi、g

---
+++ src/fffc_constants.f90
@@ -0,0 +1,5 @@
+module fffc_constants
+    use fffc_kinds
+    real(kind=real_kind), parameter :: pi = acos(-1.0_real_kind)
+    real(kind=real_kind), parameter :: g = 9.80665_real_kind
+end module fffc_constants
