ae79d9a8d1376769318ce176b6b71b0845dc9f1e
zoziha <zuo.zhihua@qq.com>
2023-03-30 18:28:45 +0800

    增加rad2deg、deg2rad

--- src/fffc_constants.f90
+++ src/fffc_constants.f90
@@ -2,4 +2,6 @@ module fffc_constants
     use fffc_kinds
     real(kind=real_kind), parameter :: pi = acos(-1.0_real_kind)
     real(kind=real_kind), parameter :: g = 9.80665_real_kind
+    real(kind=real_kind), parameter :: rad2deg = 180.0_real_kind/pi
+    real(kind=real_kind), parameter :: deg2rad = pi/180.0_real_kind
 end module fffc_constants
