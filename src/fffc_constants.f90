cd526df2de4b081275fc8f35add6578165387ff3
zoziha <zuo.zhihua@qq.com>
2023-05-15 22:06:47 +0800

    增加sqrt_eps

--- src/fffc_constants.f90
+++ src/fffc_constants.f90
@@ -1,7 +1,8 @@
 module fffc_constants
-    use fffc_kinds
+    use fffc_kinds, only: fffc_real_kind
     real(kind=fffc_real_kind), parameter :: pi = acos(-1.0_fffc_real_kind)
     real(kind=fffc_real_kind), parameter :: g = 9.80665_fffc_real_kind
     real(kind=fffc_real_kind), parameter :: rad2deg = 180.0_fffc_real_kind/pi
     real(kind=fffc_real_kind), parameter :: deg2rad = pi/180.0_fffc_real_kind
+    real(kind=fffc_real_kind), parameter :: sqrt_eps = sqrt(epsilon(1.0_fffc_real_kind))
 end module fffc_constants
