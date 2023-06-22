8ab8ae0dfdea25028a8f04ee8940400b82b3f38d
zoziha <zuo.zhihua@qq.com>
2023-06-22 21:34:33 +0800

    更新进度条

--- test/example_progress_bar.f90
+++ test/example_progress_bar.f90
@@ -1,21 +1,24 @@
 program example_progress_bar
 
-    use fffc_module, only: terminal_obj, stdout_flush, stdout_newline
+    use fffc_module, only: terminal_obj, CR
     implicit none
     integer :: i
 
     do i = 1, 10
-        call stdout_flush()
-        write (*, '(a)', advance='no') terminal_obj%progress_bar(real(i)/10)
+        write (*, '(2a)', advance='no') CR, terminal_obj%progress_bar(real(i)/10)
         write (*, '(", ", a)', advance='no') terminal_obj%alive_bar()
     end do
-    call stdout_newline()
+    print *, ''
 
     do i = 1, 10
-        call stdout_flush()
-        write (*, '(a)', advance='no') terminal_obj%progress_bar(real(i)/10)
+        write (*, '(2a)', advance='no') CR, terminal_obj%progress_bar(real(i)/10)
         write (*, '(", ", a)', advance='no') terminal_obj%alive_bar()
-        call stdout_newline()
+        print *, ''
+    end do
+
+    do i = 1, 10
+        call terminal_obj%bar(i, 10)
+        print *, ''
     end do
 
 end program example_progress_bar
