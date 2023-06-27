80fb5aaa7b448b101b3b61742531f97618b7441c
ZUO Zhihua <zuo.zhihua@qq.com>
2023-06-27 15:26:57 +0800

    更新display

--- src/fffc_io.f90
+++ src/fffc_io.f90
@@ -4,34 +4,41 @@ module fffc_io
     use fffc_kinds, only: rk => fffc_real_kind
     use fffc_string, only: to_string
     use, intrinsic :: iso_c_binding, only: nl => c_new_line
+    use, intrinsic :: iso_fortran_env, only: output_unit
     implicit none
 
     private
     public :: display
 
-    !> 在屏幕输出浮点型数组, 支持标量, 向量, 矩阵
+    !> Output floating point array on screen, support scalar, vector, matrix
+    !> @note Recommend format to use `fw.d`, `esw.d`, `ew.d`, `sp,fw.d` style, not `gw.d` style
     interface display
         module procedure :: display_0d
         module procedure :: display_1d
         module procedure :: display_2d
     end interface display
 
+    character(*), parameter :: default_format = 'es10.3'  !! default format string
+    integer, parameter :: default_length = 64  !! default string length
+
 contains
 
-    subroutine display_0d(re, header, brief, format)
-        real(rk), intent(in) :: re
-        character(*), intent(in), optional :: header
-        logical, intent(in), optional :: brief
-        character(*), intent(in), optional :: format
+    !> Output floating point scalar on screen
+    subroutine display_0d(re, header, brief, format, unit)
+        real(rk), intent(in) :: re  !! scalar
+        character(*), intent(in), optional :: header  !! header string
+        logical, intent(in), optional :: brief  !! brief output, default is `.true.`
+        character(*), intent(in), optional :: format  !! format string, default is `es10.3`
+        integer, intent(in), optional :: unit  !! output unit, default is `output_unit`
         logical :: brief_
         character(:), allocatable :: str
-        character(:), allocatable :: header_
         character(:), allocatable :: format_
+        integer :: unit_
 
         if (present(format)) then
             format_ = format
         else
-            format_ = 'es10.3'
+            format_ = default_format
         end if
 
         if (present(brief)) then
@@ -41,32 +48,40 @@ contains
         end if
 
         if (present(header)) then
-            header_ = header
+            str = header
         else
-            header_ = ''
+            str = ''
         end if
 
-        str = '[scalar] '//header_//nl
+        if (present(unit)) then
+            unit_ = unit
+        else
+            unit_ = output_unit
+        end if
+
+        str = '[scalar] '//str//nl
         str = str//to_string(re, format_)
 
-        write (*, '(a)') str
+        write (unit_, '(a)') str
 
     end subroutine display_0d
 
-    subroutine display_1d(re, header, brief, format)
-        real(rk), intent(in) :: re(:)
-        character(*), intent(in), optional :: header
-        logical, intent(in), optional :: brief
-        character(*), intent(in), optional :: format
+    !> Output floating point vector on screen
+    subroutine display_1d(re, header, brief, format, unit)
+        real(rk), intent(in) :: re(:)  !! vector
+        character(*), intent(in), optional :: header  !! header string
+        logical, intent(in), optional :: brief  !! brief output, default is `.true.`
+        character(*), intent(in), optional :: format  !! format string, default is `es10.3`
+        integer, intent(in), optional :: unit  !! output unit, default is `output_unit`
         logical :: brief_
         character(:), allocatable :: str
-        character(:), allocatable :: header_
         character(:), allocatable :: format_
+        integer :: unit_
 
         if (present(format)) then
             format_ = format
         else
-            format_ = 'es10.3'
+            format_ = default_format
         end if
 
         if (present(brief)) then
@@ -76,33 +91,41 @@ contains
         end if
 
         if (present(header)) then
-            header_ = header
+            str = header
         else
-            header_ = ''
+            str = ''
         end if
 
-        str = '[vector: '//to_string(size(re), 'i0')//'] '//header_//nl
+        if (present(unit)) then
+            unit_ = unit
+        else
+            unit_ = output_unit
+        end if
+
+        str = '[vector: '//to_string(size(re), 'i0')//'] '//str//nl
         call vector_string(re, brief_, format_, str)
 
-        write (*, '(a)') str
+        write (unit_, '(a)') str
 
     end subroutine display_1d
 
-    subroutine display_2d(re, header, brief, format)
-        real(rk), intent(in) :: re(:, :)
-        character(*), intent(in), optional :: header
-        logical, intent(in), optional :: brief
-        character(*), intent(in), optional :: format
+    !> Output floating point matrix on screen
+    subroutine display_2d(re, header, brief, format, unit)
+        real(rk), intent(in) :: re(:, :)  !! matrix
+        character(*), intent(in), optional :: header  !! header string
+        logical, intent(in), optional :: brief  !! brief output, default is `.true.`
+        character(*), intent(in), optional :: format  !! format string, default is `es10.3`
+        integer, intent(in), optional :: unit  !! output unit, default is `output_unit`
         logical :: brief_
         character(:), allocatable :: str
-        character(:), allocatable :: header_
         character(:), allocatable :: format_
+        integer :: unit_
         integer :: i
 
         if (present(format)) then
             format_ = format
         else
-            format_ = 'es10.3'
+            format_ = default_format
         end if
 
         if (present(brief)) then
@@ -112,12 +135,18 @@ contains
         end if
 
         if (present(header)) then
-            header_ = header
+            str = header
         else
-            header_ = ''
+            str = ''
         end if
 
-        str = '[matrix: '//to_string(size(re, 1), 'i0')//'*'//to_string(size(re, 2), 'i0')//'] '//header_//nl
+        if (present(unit)) then
+            unit_ = unit
+        else
+            unit_ = output_unit
+        end if
+
+        str = '[matrix: '//to_string(size(re, 1), 'i0')//'*'//to_string(size(re, 2), 'i0')//'] '//str//nl
         if (brief_ .and. size(re, 1) > 5) then
             do i = 1, 3
                 call vector_string(re(i, :), brief_, format_, str)
@@ -133,10 +162,11 @@ contains
             call vector_string(re(size(re, 1), :), brief_, format_, str)
         end if
 
-        write (*, '(a)') str
+        write (unit_, '(a)') str
 
     end subroutine display_2d
 
+    !> Private routine: Convert floating point vector to string
     subroutine vector_string(x, brief, format, string)
         real(rk), intent(in) :: x(:)
         logical, intent(in) :: brief
--- test/test_fffc_io.f90
+++ test/test_fffc_io.f90
@@ -25,10 +25,12 @@ contains
         integer :: i
 
         call display(1.0_rk, '1.0:')
+        call display(1.0_rk, '1.0:', format='sp,f10.3', unit=6) ! default format is 'es10.3'
         call display([real(rk) ::(i, i=1, 10)], '1:10:')
-        call display(reshape([real(rk) ::(i, i=1, 36)], [6, 6]), '6x6:', .true.)
-        call display(reshape([real(rk) ::(i, i=1, 36)], [6, 5]), '6x5:', .false.)
+        call display([real(rk) ::(i, i=1, 10)], header='1:10:', brief=.false.)
         call display(reshape([real(rk) ::(i, i=1, 25)], [5, 5]), '5x5:')
+        call display(reshape([real(rk) ::(i, i=1, 36)], [6, 6]), '6x6:', .true.)
+        call display(reshape([real(rk) ::(i, i=1, -35, -1)], [6, 6]), '6x6:', .false.)
 
     end subroutine test_display
 
